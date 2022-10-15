8747764d50ae7c447fe5b18ae9480baa340b12ab
CharKwehTiawKerang <62492138+CharKwehTiawKerang@users.noreply.github.com>
2022-10-15 17:53:50 +0800

    Add files via upload

--- car-database.js
+++ car-database.js
@@ -307,49 +307,38 @@ const car = {
                 "Kelisa": [
                     {
                         "Battery": {
-                            "Default": "(B20L)"
+                            "Default": "NS40ZL (38B20L)"
                         },
-                        "Brake Fluid": "DOT 3",
+                        "Brake Fluid": "DOT 3 / DOT 4",
                         "Bulb": {
-                            "Default": "H11 (55 Watt)",
-                            "Alternative": "H8 / H9 / H16"
+                            "Default": "H4 (60/55 Watt)",
+                            "Alternative": "HB2"
                         },
                         "Coolant": {
-                            "Quantity": {
-                                "Bezza 1.0 - Manual (2016)": "2.6 Liter",
-                                "Bezza 1.0 - Auto (2016)": "2.9 Liter"
-                            }
+                            "Quantity":  "3 Liter"
                         },
                         "Engine Oil": {
-                            "Type": "0W-20 (Fully Synthetic)",
-                            "Quantity": {
-                                "Bezza 1.3 (2020)": "3.5 Liter (With oil filter), 3.3 Liter (Without oil filter)"
-                            }
+                            "Type": "10W-30 (Mineral)",
+                            "Quantity":  "2.3 Liter (With oil filter), 2.1 Liter (Without oil filter)"
                         },
-                        "Fan Belt": {
-                            "Bezza 1.3 (2020)": "6PK1162"
-                        },
+                        "Fan Belt":  "4PK715",
                         "Gear Oil": {
                             "Type": {
-                                "Auto": "ATF D3 SP",
-                                "Manual": "Gear Oil GL 4 80W"
+                                "Auto": "ATF D3 (Dexron III)",
+                                "Manual": "Gear Oil GL 3 / GL 4 75W-90"
                             },
                             "Quantity": {
-                                "Auto": "3 Liter (1 Liter x 3 Bottles)",
-                                "Manual": "2 Liter (1 Liter x 2 Bottles)"
+                                "Auto": "4 Liter (1 Liter x 4 Bottles)",
+                                "Manual": "2.25 Liter (1 Liter x 3 Bottles)"
                             }
                         },
                         "Spark Plug": {
-                            "Type": {
-                                "Bezza 1.3 (2020)": "SC16HR11 (Denso)"
-                            },
-                            "Quantity": {
-                                "Bezza 1.3 (2020)": "4 Spark Plug"
-                            }
+                            "Type":  "BKR5E-11 (NGK)",
+                            "Quantity": "3 Spark Plug"
                         },
                         "Wiper": {
                             "Type": "U-Hook",
-                            "Size": "21” (Driver), 14” (Passenger), N/A (Rear)"
+                            "Size": "18” (Driver), 16” (Passenger), 14” (Rear)"
                         }
                     }
                 ],
@@ -405,49 +394,38 @@ const car = {
                 "Kenari": [
                     {
                         "Battery": {
-                            "Default": "(B20L)"
+                            "Default": "NS40ZL (38B20L)"
                         },
-                        "Brake Fluid": "DOT 3",
+                        "Brake Fluid": "DOT 3 / DOT 4",
                         "Bulb": {
-                            "Default": "H11 (55 Watt)",
-                            "Alternative": "H8 / H9 / H16"
+                            "Default": "H4 (60/55 Watt)",
+                            "Alternative": "HB2"
                         },
                         "Coolant": {
-                            "Quantity": {
-                                "Bezza 1.0 - Manual (2016)": "2.6 Liter",
-                                "Bezza 1.0 - Auto (2016)": "2.9 Liter"
-                            }
+                            "Quantity":  "3 Liter"
                         },
                         "Engine Oil": {
-                            "Type": "0W-20 (Fully Synthetic)",
-                            "Quantity": {
-                                "Bezza 1.3 (2020)": "3.5 Liter (With oil filter), 3.3 Liter (Without oil filter)"
-                            }
+                            "Type": "10W-30 (Mineral)",
+                            "Quantity":  "2.3 Liter (With oil filter), 2.1 Liter (Without oil filter)"
                         },
-                        "Fan Belt": {
-                            "Bezza 1.3 (2020)": "6PK1162"
-                        },
+                        "Fan Belt":  "4PK715",
                         "Gear Oil": {
                             "Type": {
-                                "Auto": "ATF D3 SP",
-                                "Manual": "Gear Oil GL 4 80W"
+                                "Auto": "ATF D3 (Dexron III) / ATF D2 (Dexron II)",
+                                "Manual": "Gear Oil GL 3 / GL 4 (75W-90/75W-85)"
                             },
                             "Quantity": {
-                                "Auto": "3 Liter (1 Liter x 3 Bottles)",
-                                "Manual": "2 Liter (1 Liter x 2 Bottles)"
+                                "Auto": "4 Liter (1 Liter x 4 Bottles)",
+                                "Manual": "2.25 Liter (1 Liter x 3 Bottles)"
                             }
                         },
                         "Spark Plug": {
-                            "Type": {
-                                "Bezza 1.3 (2020)": "SC16HR11 (Denso)"
-                            },
-                            "Quantity": {
-                                "Bezza 1.3 (2020)": "4 Spark Plug"
-                            }
+                            "Type":  "K16PR-U11 (Denso), BKR5E-11 (NGK)",
+                            "Quantity": "3 Spark Plug"
                         },
                         "Wiper": {
                             "Type": "U-Hook",
-                            "Size": "21” (Driver), 14” (Passenger), N/A (Rear)"
+                            "Size": "20” (Driver), 17” (Passenger), 14” (Rear)"
                         }
                     }
                 ],
---
+++ product.js
@@ -0,0 +1,16 @@
+const products = {
+    "Battery": {
+        "Proton": {
+            "Perdana": ["D26R (Defaults)", "75D26R (Varta)", "90D26R (Century)"],
+            "Preve (No turbo)": ["D23L (Defaults)", "55D23L (Varta)", "55D23L (Century)"]
+        }
+    },
+    "Spark Plugs": {
+        "Proton": {
+            "GEN 2": ["BPR6EF-11 (NGK)", "IT20 (Denso)"],
+            "Perdana": [" (NGK)", " (Denso)"] 
+        }
+    }
+}
+
+exports.products = products
